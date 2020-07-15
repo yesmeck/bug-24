cfb9494167e91ea187799abd3c84335a574954f5
Meck Zhu <yesmeck@gmail.com>
2020-07-15 20:07:41 +0800

    Initial commit

---
+++ src/pages/index/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View } from 'remax/ali';
+
+const A = () => {
+  return <View>123</View>
+}
+
+export default (props) => {
+  return (
+    <>
+      <A />
+    </>
+  );
+};
